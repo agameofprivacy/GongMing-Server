385c852c451c5df67e9ebf78fa978ae4c90ac010
Eddie Chen <agameofprivacy@gmail.com>
2016-12-16 16:22:48 -0700

    decode fix

--- routes/gongMing_routes.js
+++ routes/gongMing_routes.js
@@ -9609,7 +9609,7 @@ exports.getLegislatorsWithLatLon = function(req, res){
         res.set("Access-Control-Allow-Credentials", true);
     }
     console.log(origin);
-    var webAPIKeySubmitted = decodeURICompoent(req.query.webAPIKey);
+    var webAPIKeySubmitted = decodeURIComponent(req.query.webAPIKey);
     console.log(webAPIKeySubmitted);
     var lat = Number(req.query.lat);
     var lon = Number(req.query.lon);
